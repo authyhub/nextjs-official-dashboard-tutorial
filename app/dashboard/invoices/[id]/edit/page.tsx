1e5dc2257f9e280bc4b18ac273ca9424eb368dcb
maktechdev <69475978+fullTimeCode@users.noreply.github.com>
2025-11-07 15:05:14 +0100

    modify app/dashboard/invoices/[id]/edit/page.tsx: update code to add form and breadcrumb components

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,3 +1,24 @@
-export default function Page() {
-  return <div className=''>page</div>
+import Form from '@/app/ui/invoices/edit-form'
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs'
+import { fetchCustomers } from '@/app/lib/data'
+
+export default async function Page() {
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form
+        invoice={invoice}
+        customers={customers}
+      />
+    </main>
+  )
 }
