f5213eab66d6fcf56aa5362fd00b3171132f9c7e
maktechdev <69475978+fullTimeCode@users.noreply.github.com>
2025-11-07 15:17:21 +0100

    modify app/dashboard/invoices/[id]/edit/page.tsx: add params to props and extract id from page props

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,7 +2,14 @@ import Form from '@/app/ui/invoices/edit-form'
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs'
 import { fetchCustomers } from '@/app/lib/data'
 
-export default async function Page() {
+export default async function Page(props: {
+  params: Promise<{
+    id: string
+  }>
+}) {
+  const { id } = await props.params
+
+  const data = fetchCustomers()
   return (
     <main>
       <Breadcrumbs
