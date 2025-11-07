46da84efc7791495a67c6eda463f24c98f8c87d1
maktechdev <69475978+fullTimeCode@users.noreply.github.com>
2025-11-07 18:53:06 +0100

    modify app/lib/actions.ts: complete the updateInvoice server action

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -36,5 +36,26 @@ export async function createInvoice(formData: FormData) {
   // Once the database has been updated, the /dashboard/invoices path will be revalidated, and fresh data will be fetched from the server.
   redirect('/dashboard/invoices')
 }
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true })
 
-export async function updateInvoice(formData: FormData) {}
+// ...
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  })
+
+  const amountInCents = amount * 100
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `
+
+  revalidatePath('/dashboard/invoices')
+  redirect('/dashboard/invoices')
+}
